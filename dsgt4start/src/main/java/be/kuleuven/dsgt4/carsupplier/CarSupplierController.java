f4bc4130acd3adf188ccc0c05f8f45520edd9978
arnoud <arnoud.stas@student.kuleuven.be>
2024-05-19 18:31:16 +0200

    code for CarSupplierController

--- dsgt4start/src/main/java/be/kuleuven/dsgt4/carsupplier/CarSupplierController.java
+++ dsgt4start/src/main/java/be/kuleuven/dsgt4/carsupplier/CarSupplierController.java
@@ -1,4 +1,29 @@
 package be.kuleuven.dsgt4.carsupplier;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/cars")
 public class CarSupplierController {
+    @Autowired
+    private CarService carService;
+
+    @GetMapping
+    public List<Car> getAllCars() {
+        return carService.getAllCars();
+    }
+
+    @GetMapping("/{id}")
+    public Car getCarById(@PathVariable UUID id) {
+        return carService.getCarById(id);
+    }
+
+    @PostMapping("/order")
+    public OrderResponse orderCar(@RequestBody CarOrderRequest orderRequest) {
+        return carService.orderCar(orderRequest);
+    }
 }
