aef5ecd9e40a930cb7d688fc9a3739151b3ad5cf
arnoud <arnoud.stas@student.kuleuven.be>
2024-05-19 22:39:10 +0200

    added hypermedia links

--- dsgt4start/src/main/java/be/kuleuven/dsgt4/carsupplier/CarSupplierController.java
+++ dsgt4start/src/main/java/be/kuleuven/dsgt4/carsupplier/CarSupplierController.java
@@ -1,5 +1,8 @@
 package be.kuleuven.dsgt4.carsupplier;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,17 +16,32 @@ public class CarSupplierController {
     private CarService carService;
 
     @GetMapping
-    public List<Car> getAllCars() {
-        return carService.getAllCars();
+    public CollectionModel<EntityModel<Car>> getAllCars() {
+        List<EntityModel<Car>> cars = carService.getAllCars().stream()
+                .map(car -> EntityModel.of(car,
+                        linkTo(methodOn(CarSupplierController.class).getCarById(car.getId())).withSelfRel(),
+                        linkTo(methodOn(CarSupplierController.class).getAllCars()).withRel("cars")))
+                .toList();
+        return CollectionModel.of(cars, linkTo(methodOn(CarSupplierController.class).getAllCars()).withSelfRel());
+
     }
 
     @GetMapping("/{id}")
-    public Car getCarById(@PathVariable UUID id) {
-        return carService.getCarById(id);
+    public EntityModel<Car> getCarById(@PathVariable UUID id) {
+        Car car = carService.getCarById(id);
+        if (car == null) {
+            return EntityModel.of(null, linkTo(methodOn(CarSupplierController.class).getAllCars()).withRel("cars"));
+        }
+        return EntityModel.of(car,
+                linkTo(methodOn(CarSupplierController.class).getCarById(id)).withSelfRel(),
+                linkTo(methodOn(CarSupplierController.class).getAllCars()).withRel("cars"));
     }
 
     @PostMapping("/order")
-    public OrderResponse orderCar(@RequestBody CarOrderRequest orderRequest) {
-        return carService.orderCar(orderRequest);
+    public EntityModel<OrderResponse> orderCar(@RequestBody CarOrderRequest orderRequest) {
+        OrderResponse response = carService.orderCar(orderRequest);
+        return EntityModel.of(response,
+                linkTo(methodOn(CarSupplierController.class).orderCar(orderRequest)).withSelfRel(),
+                linkTo(methodOn(CarSupplierController.class).getAllCars()).withRel("cars"));
     }
 }
